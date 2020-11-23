2b8bac4c33ddd865b596f8ca159b969daa3be18b
Nay Min Htwe <konayminhtwe@gmail.com>
2020-11-23 23:38:47 +0630

    category type

--- core/modules/category/store/getter.js
+++ core/modules/category/store/getter.js
@@ -1,7 +1,13 @@
 const getters = {
   allCategories (state) {
     return state.list
-  }
+  },
+  specialCategories (state, getters) {
+    return getters.allCategories.filter(category => category.web_url)
+  },
+  normalCategories (state, getters) {
+    return getters.allCategories.filter(category => !category.web_url)
+  },
 }
 
 export default getters
