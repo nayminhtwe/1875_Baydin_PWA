ce54a9a572bd0912b9b09cb0a92ed1719e22f642
Nay Min Htwe <konayminhtwe@gmail.com>
2020-12-04 13:20:24 +0630

    get content from api #1

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -2,27 +2,29 @@
   <section class="ds s-pt-90 s-pb-40 s-pt-xl-150 s-pb-xl-100 c-gutter-60 c-mb-50">
     <div class="container">
       <div class="row">
-
-						<div class="col-12">
-							<div class="vertical-item bg-darkblue text-center content-padding padding-big">
-								<div class="item-media">
-									<img :src="getCurrentCategory.image" alt="img">
-								</div>
-								<div class="item-content max-content">
-									<p v-for="content in contents" :key="content.id">
-										{{ JSON.parse(content.content) }}
-									</p>
-								</div>
-							</div>
-							<div class="share_buttons social_part">
-								<a href="https://www.facebook.com" class="color-bg-icon fa fa-facebook" target="_blank"></a>
-								<a href="https://twitter.com" class="color-bg-icon fa fa-twitter" target="_blank"></a>
-								<a href="https://pinterest.com" class="color-bg-icon fa fa-pinterest" target="_blank"></a>
-								<a href="https://instagram.com" class="color-bg-icon fa fa-instagram" target="_blank"></a>
-								<a href="https://www.youtube.com" class="color-bg-icon fa fa-youtube-play" target="_blank"></a>
-							</div>
-						</div><!-- .col-* -->
-					</div>
+        <div class="col-12">
+          <div class="vertical-item bg-darkblue text-center content-padding padding-big">
+            <div class="item-media">
+              <img :src="getCurrentCategory.image" alt="img" />
+            </div>
+            <div class="item-content max-content" v-if="contents.length !== 0">
+              <p v-for="content in contents" :key="content.id">{{ JSON.parse(content.content) }}</p>
+            </div>
+          </div>
+          <div class="share_buttons social_part">
+            <a href="https://www.facebook.com" class="color-bg-icon fa fa-facebook" target="_blank"></a>
+            <a href="https://twitter.com" class="color-bg-icon fa fa-twitter" target="_blank"></a>
+            <a href="https://pinterest.com" class="color-bg-icon fa fa-pinterest" target="_blank"></a>
+            <a href="https://instagram.com" class="color-bg-icon fa fa-instagram" target="_blank"></a>
+            <a
+              href="https://www.youtube.com"
+              class="color-bg-icon fa fa-youtube-play"
+              target="_blank"
+            ></a>
+          </div>
+        </div>
+        <!-- .col-* -->
+      </div>
     </div>
   </section>
 </template>
@@ -34,22 +36,26 @@ import { HTTP } from "@core/lib/http-common";
 export default {
   name: `Normal`,
   data() {
-	return {
-		contents: [],
-	}
+    return {
+      contents: []
+    };
   },
   computed: {
     ...mapGetters({
       getCurrentCategory: "categoryStore/getCurrentCategory"
     })
   },
-  created() {
-    this.$store.dispatch("categoryStore/singleCategory", this.$route.params.categoryId);
-  },
-  mounted() {
-	HTTP.get(`content?category_id=1&content_category_id=173`).then((response) => {
-		this.contents = response.data.data;
+  async created() {
+    await this.$store.dispatch(
+      "categoryStore/singleCategory",
+      this.$route.params.categoryId
+	);
+	HTTP.get(
+      `content?category_id=1&content_category_id=180`
+    ).then(response => {
+      console.log(this.getCurrentCategory.id, response);
+      this.contents = response.data.data;
     });
-  }
+  },
 };
 </script>
