f9631ce36f2295fb7c07a9f84ecbfc27644c55a0
naymin <konayminhtwe@gmail.com>
2021-06-17 15:04:20 +0630

    startPay test #2

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,21 +1,47 @@
 <template>
-  <section class="ds s-pt-90 s-pb-40 s-pt-xl-150 s-pb-xl-100 c-gutter-60 c-mb-50">
+  <section
+    class="ds s-pt-90 s-pb-40 s-pt-xl-150 s-pb-xl-100 c-gutter-60 c-mb-50"
+    @click="startPay()"
+  >
     <div class="container">
       <div class="row">
         <div class="col-12">
           <div class="vertical-item bg-darkblue text-center content-padding padding-big">
             <div class="item-media">
-              <img :src="getCurrentCategory.image" alt="img" v-if="getCurrentCategory.image" />
+              <img
+                :src="getCurrentCategory.image"
+                alt="img"
+                v-if="getCurrentCategory.image"
+              />
             </div>
             <div class="item-content max-content">
-              <p v-for="content in getContents" :key="content.id">{{ JSON.parse(content.content)[0] }}</p>
+              <p
+                v-for="content in getContents"
+                :key="content.id"
+              >{{ JSON.parse(content.content)[0] }}</p>
             </div>
           </div>
           <div class="share_buttons social_part">
-            <a href="https://www.facebook.com" class="color-bg-icon fa fa-facebook" target="_blank"></a>
-            <a href="https://twitter.com" class="color-bg-icon fa fa-twitter" target="_blank"></a>
-            <a href="https://pinterest.com" class="color-bg-icon fa fa-pinterest" target="_blank"></a>
-            <a href="https://instagram.com" class="color-bg-icon fa fa-instagram" target="_blank"></a>
+            <a
+              href="https://www.facebook.com"
+              class="color-bg-icon fa fa-facebook"
+              target="_blank"
+            ></a>
+            <a
+              href="https://twitter.com"
+              class="color-bg-icon fa fa-twitter"
+              target="_blank"
+            ></a>
+            <a
+              href="https://pinterest.com"
+              class="color-bg-icon fa fa-pinterest"
+              target="_blank"
+            ></a>
+            <a
+              href="https://instagram.com"
+              class="color-bg-icon fa fa-instagram"
+              target="_blank"
+            ></a>
             <a
               href="https://www.youtube.com"
               class="color-bg-icon fa fa-youtube-play"
@@ -41,11 +67,16 @@ export default {
       getContents: "categoryStore/getContents"
     })
   },
-  created() {
+  created () {
     this.$store.dispatch(
       "categoryStore/getContents",
       this.$route.params.categoryId
     );
+  },
+  methods: {
+    startPay () {
+      this.kbzpay.startPay('KBZ00261c56010070436571da196b74a56a04140644911', 'appid=kp9539d5120b864436980c3f25966215&merch_code=200170&nonce_str=gXvnSZWWrJjqbkHUXBdlRubrTMKeLuzc&prepay_id=KBZ00261c56010070436571da196b74a56a04140644911&timestamp=1535166225&key=zKyBnbqZYQ0P^H^4SSaBu3Bw&hsJAYlZ', 'a6583b507f7f88ecae445faa9980dca8723f0f5b16b9032f47a83d9f332d32ce', {}, {})
+    }
   }
 };
 </script>
