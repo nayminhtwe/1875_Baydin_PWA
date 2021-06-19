03a6458e91a89d6e134779761c0588548a3577ef
naymin <konayminhtwe@gmail.com>
2021-06-19 16:33:13 +0630

    remove testing

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -22,7 +22,6 @@
             <button
               name="pay"
               style="margin-left: 120px;"
-              @click="startPay()"
             >Pay Now</button>
           </div>
         </div>
@@ -49,11 +48,6 @@ export default {
       "categoryStore/getContents",
       this.$route.params.categoryId
     );
-  },
-  methods: {
-    startPay () {
-      this.kbzpay.startPay('KBZ00261c56010070436571da196b74a56a04140644911', 'appid=kp9539d5120b864436980c3f25966215&merch_code=200170&nonce_str=gXvnSZWWrJjqbkHUXBdlRubrTMKeLuzc&prepay_id=KBZ00261c56010070436571da196b74a56a04140644911&timestamp=1535166225&key=zKyBnbqZYQ0P^H^4SSaBu3Bw&hsJAYlZ', 'a6583b507f7f88ecae445faa9980dca8723f0f5b16b9032f47a83d9f332d32ce', {}, {})
-    }
   }
 };
 </script>
