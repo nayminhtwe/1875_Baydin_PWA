80afe620177e4e1b94d7bf827d29d9dbd294a136
naymin <konayminhtwe@gmail.com>
2021-07-12 20:57:36 +0630

    1875 Bay Din with vue-meta

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,68 +1,60 @@
 <template>
-    <div id="canvas">
-        <div id="box_wrapper">
-            <main-header/>
-            <main class="LayoutDefault__main">
-                <slot/>
-            </main>
-            <main-footer/>
-        </div>
+  <div id="canvas">
+    <div id="box_wrapper">
+      <main-header />
+      <main class="LayoutDefault__main">
+        <slot />
+      </main>
+      <main-footer />
     </div>
+  </div>
 </template>
 
 <script>
-    import MainHeader from '@/components/core/blocks/Header/Header.vue'
-    import MainFooter from '@/components/core/blocks/Footer/Footer.vue'
+import MainHeader from '@/components/core/blocks/Header/Header.vue'
+import MainFooter from '@/components/core/blocks/Footer/Footer.vue'
 
-    export default {
-        name: `LayoutDefault`,
-        data() {
-            return {
-                user: null,
-            };
-        },
-        created() {
-            // Simulate fetching user data.
-            setTimeout(() => {
-                this.user = {name: `John Doe`};
-            }, 2000);
-        },
-        components: {
-            MainHeader,
-            MainFooter
-        }
-    };
+export default {
+  name: `LayoutDefault`,
+  metaInfo: {
+    title: '1875 Bay Din'
+  },
+  components: {
+    MainHeader,
+    MainFooter
+  }
+};
 </script>
 
 <style lang="scss">
-    /*.LayoutDefault {*/
-    /*max-width: 42em;*/
-    /*margin-right: auto;*/
-    /*margin-left: auto;*/
-    /*padding-right: 1em;*/
-    /*padding-left: 1em;*/
+/*.LayoutDefault {*/
+/*max-width: 42em;*/
+/*margin-right: auto;*/
+/*margin-left: auto;*/
+/*padding-right: 1em;*/
+/*padding-left: 1em;*/
 
-    /*&__nav {*/
-    /*padding-top: 1em;*/
-    /*padding-bottom: 1em;*/
-    /*border-bottom: 1px solid #c0c0c0;*/
-    /*}*/
+/*&__nav {*/
+/*padding-top: 1em;*/
+/*padding-bottom: 1em;*/
+/*border-bottom: 1px solid #c0c0c0;*/
+/*}*/
 
-    /*&__user {*/
-    /*float: right;*/
-    /*}*/
+/*&__user {*/
+/*float: right;*/
+/*}*/
 
-    /*&__main {*/
-    /*padding-top: 1.5em;*/
-    /*padding-bottom: 2em;*/
-    /*}*/
+/*&__main {*/
+/*padding-top: 1.5em;*/
+/*padding-bottom: 2em;*/
+/*}*/
 
-    /*&__footer {*/
-    /*padding-top: 1em;*/
-    /*padding-bottom: 1em;*/
-    /*border-top: 1px solid #c0c0c0;*/
-    /*}*/
-    /*}*/
+/*&__footer {*/
+/*padding-top: 1em;*/
+/*padding-bottom: 1em;*/
+/*border-top: 1px solid #c0c0c0;*/
+/*}*/
+/*}*/
 </style>
 <style lang="css" src="@/css/bootstrap.min.css"></style>
 <!--<style lang="css" src="@/css/animations.css"></style>-->
--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import LoadScript from 'vue-plugin-load-script';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import moment from 'moment'
+import Meta from 'vue-meta'
 
 Vue.config.productionTip = false
 
 Vue.use(LoadScript);
 Vue.use(VueSweetalert2);
+Vue.use(Meta)
 
 Vue.prototype.moment = moment
 
